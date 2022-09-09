db447fd3022b6a7d7d48f7f51ab0af078c9dc00e
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-09-09 18:34:54 +0100

    Initial commit

---
+++ CarefreeLuck/CarefreeLuck.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Modding;
+using MonoMod.Cil;
+using UnityEngine;
+
+namespace CarefreeLuck
+{
+    public class CarefreeLuck : Mod
+    {
+        internal static CarefreeLuck instance;
+        
+        public CarefreeLuck() : base(null)
+        {
+            instance = this;
+        }
+        
+        public override string GetVersion()
+        {
+            return GetType().Assembly.GetName().Version.ToString();
+        }
+        
+        public override void Initialize()
+        {
+            Log("Initializing Mod...");
+
+            IL.HeroController.TakeDamage += HeroController_TakeDamage;
+        }
+
+        private void HeroController_TakeDamage(ILContext il)
+        {
+            ILCursor cursor = new(il);
+
+            while (cursor.TryGotoNext(MoveType.After, i => i.MatchCallOrCallvirt(typeof(UnityEngine.Random), "Range")))
+            {
+                cursor.EmitDelegate<Func<int, int>>(x => 100);
+            }
+        }
+    }
+}
